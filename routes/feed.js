e946e485fc0585ef79e1822d0b8af01f2b4559b0
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2026-02-15 22:53:35 +0200

    JWT Authentication, /auth/login route and is-auth middleware

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { body } = require("express-validator");
-
+const isAuth = require('../middleware/is-auth');
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
@@ -13,18 +13,21 @@ router.post(
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.createPost,
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
+
 router.put(
   "/post/:postId",
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.updatePost,
 );
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
